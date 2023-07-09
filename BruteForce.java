3efec893ca93f1e680263edf7a4c212cd6b0b1ac
Mbissine2022 <bebembiss@gmail.com>
2023-07-09 18:23:53 +0000

    update brute force

--- BruteForce.java
+++ BruteForce.java
@@ -1,108 +1,122 @@
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.math.BigInteger;
 
-public class BruteForce {
-    public String craquerSimple(String password) {
+public class BruteForce extends Cracker {
+    public String craquerSimple(String password){
+        // implémentation 
         String foundPassword;
         char[] chaine = new char[password.length()];
         System.out.println("craquage simple appelé");
         foundPassword = bruteForce(password, chaine, 0);
-        if (foundPassword == null) {
-            System.out.println("Bravo, nous n'avons pas pu craquer ton mot de passe !");
-        } else {
-            System.out.println("Ohh! Ton mot de passe est " + foundPassword);
+        if(foundPassword == null ){
+            System.out.println("Bravo nous n'avons pas pu craque ton mot de passe !");
+        }else{
+            System.out.println("Ohh! sa mot de passe baaxutt. sa mot de passe pi mooy "+foundPassword);
         }
+
+
         return null;
     }
-    
 
-    public String bruteForce(String password, char[] chaine, int position) {
-        if (position == password.length() - 1) {
-            for (int i = 97; i < 97 + 26; i++) {
-                chaine[position] = Character.toLowerCase((char) i);
+
+    public String bruteForce(String password, char[] chaine, int position){
+        
+        if(position == password.length() - 1){
+            // condition d'arret
+             for(int i = 97; i< 97+26; i++){
+                chaine[position] = (char)i;
                 String combinaison = new String(chaine);
-                System.out.println("La chaîne générée est : " + combinaison);
-                if (password.equals(combinaison)) {
-                    System.out.println("Ton mot de passe est : " + combinaison);
+                System.out.println("la chaine genere est : "+combinaison);
+                if(password.equals(combinaison)){
+                    System.out.println("votre mot de pass est :  "+combinaison);
                     return combinaison;
-                }
+                } 
+
             }
             return null;
-        } else {
-            for (int i = 97; i < 97 + 26; i++) {
-                chaine[position] = Character.toLowerCase((char) i);
-                chaine = chaine.clone();
-                if (bruteForce(password, chaine, position + 1) != null) {
+
+        }else{
+            // pas encore arrivé à la dernière lettre
+            for(int i = 97; i< 97+26; i++){
+                chaine[position] = (char)i;
+                if(bruteForce(password, chaine, position+1)!= null){
                     return new String(chaine);
                 }
+                
             }
             return null;
         }
     }
 
-    public String craquerHashed(String password) throws NoSuchAlgorithmException {
-        String foundPassword;
+
+    public String craquerHashed(String password){
+        
         byte[] md = Hash(password);
+        String foundPassword;
         char[] chaine = new char[md.length];
-        System.out.println("craquage haché appelé");
+        System.out.println("craquer hache appelé");
         foundPassword = bruteForceH(password, md, chaine, 0);
-        if (foundPassword == null) {
-            System.out.println("Bravo, nous n'avons pas pu cracker ton mot de passe !");
-        } else {
-            System.out.println("Ohh! Ton mot de passe est : " + foundPassword);
+        if(foundPassword == null ){
+            System.out.println("Bravo nous n'avons pas pu cracke ton mot de passe !");
+        }else{
+            System.out.println("Ohh! sa mot de passe baaxutt. sa mot de passe pi mooy "+foundPassword);
         }
+
+
         return null;
+
     }
 
-    public String bruteForceH(String password, byte[] md, char[] chaine, int position) throws NoSuchAlgorithmException {
-        if (position == password.length() - 1) {
-            for (int i = 97; i < 97 + 26; i++) {
-                chaine[position] = Character.toLowerCase((char) i);
+    public String bruteForceH(String password, byte[] md, char[] chaine, int position){
+        
+        if(position == password.length() - 1){
+            // condition d'arret
+             for(int i = 97; i< 97+26; i++){
+                chaine[position] = (char)i;
                 String combinaison = new String(chaine);
                 byte[] test = Hash(combinaison);
-                if (MessageDigest.isEqual(test, md)) {
-                    System.out.println("Ton mot de passe est : " + combinaison);
+                System.out.println("test : " + test);
+                System.out.println("md :" +md);
+        
+                if(MessageDigest.isEqual(test,md)){
+                    System.out.println("votre mot de passe est :  "+combinaison);
                     return combinaison;
-                }
+                } 
+
             }
             return null;
-        } else {
-            for (int i = 97; i < 97 + 26; i++) {
-                chaine[position] = Character.toLowerCase((char) i);
-                chaine = chaine.clone();
-                if (bruteForceH(password, md, chaine, position + 1) != null) {
+
+        }else{
+            // pas encore arrivé à la dernière lettre
+            System.out.println("on est dans le else");
+            for(int i = 97; i< 97+26; i++){
+                chaine[position] = (char)i;
+                if(bruteForceH(password, md, chaine, position+1)!= null){
                     return new String(chaine);
                 }
+                
             }
             return null;
         }
     }
 
-    public byte[] Hash(String input) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] messageDigest = md.digest(input.getBytes(StandardCharsets.UTF_8));
-        return messageDigest;
-    }
 
-    public static void main(String[] args) {
-        BruteForce cracker = new BruteForce();
-        try {
-            String password = "asdf123";
-            cracker.craquerSimple(password);
 
-            String hashedPassword = cracker.encryptString(password);
-            cracker.craquerHashed(hashedPassword);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+        static byte[] Hash(String input){
+            byte[] output=null;
+          
+                MessageDigest digest;
+                try {
+                    digest = MessageDigest.getInstance("SHA-1");
+                    output = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+                } catch (NoSuchAlgorithmException e) {
+
+                    e.printStackTrace();
+                }
+                
+                return output;
         }
-    }
 
-    public String encryptString(String input) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] messageDigest = md.digest(input.getBytes());
-        BigInteger bigInt = new BigInteger(1, messageDigest);
-        return bigInt.toString(16);
+
     }
-}
